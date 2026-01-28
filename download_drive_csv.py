e65192216742ff50935a46c2cfff0c14d6a1dfa6
TristanChew <tristanchewzy@gmail.com>
2026-01-28 14:23:22 +0800

    Create download_drive_csv.py

---
+++ download_drive_csv.py
@@ -0,0 +1,25 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+import io
+from googleapiclient.http import MediaIoBaseDownload
+
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+creds = service_account.Credentials.from_service_account_file(
+    'credentials.json', scopes=SCOPES)
+
+service = build('drive', 'v3', credentials=creds)
+
+FOLDER_ID = 'YOUR_FOLDER_ID'
+
+results = service.files().list(
+    q=f"'{FOLDER_ID}' in parents and mimeType='text/csv'",
+    fields="files(id, name)"
+).execute()
+
+for file in results['files']:
+    request = service.files().get_media(fileId=file['id'])
+    fh = io.FileIO(file['name'], 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
